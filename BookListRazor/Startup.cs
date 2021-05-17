1fd7a706db8cbd65818630b464609d977ed0fb9b
vineet39 <vinit.bugtani@gmail.com>
2021-05-17 20:18:52 +0530

    Completed CRUD app

--- BookListRazor/Startup.cs
+++ BookListRazor/Startup.cs
@@ -25,6 +25,8 @@ namespace BookListRazor
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // added for web api
+            services.AddControllersWithViews();
             services.AddDbContext<ApplicationDbContext>(options =>
             options.UseSqlServer(
                 Configuration.GetConnectionString("DefaultConnection")));
@@ -54,6 +56,8 @@ namespace BookListRazor
 
             app.UseEndpoints(endpoints =>
             {
+                // added for web api
+                endpoints.MapControllers();
                 endpoints.MapRazorPages();
             });
         }
